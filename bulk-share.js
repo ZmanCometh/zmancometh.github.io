5d91a8a583813b34201d3bdee5f4096c22612bd8
ZmanCometh <88464073+ZmanCometh@users.noreply.github.com>
2021-12-23 23:20:46 -0600

    Create bulk-share.js

---
+++ bulk-share.js
@@ -0,0 +1,22 @@
+var xpath = "//a[text()='Add Friend']";
+var addfriend = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+addfriend.click();
+
+members = document.querySelector("#space_share_memberships_attributes_new_record_friend_id") || document.querySelector("#kit_share_memberships_attributes_new_record_friend_id") || document.querySelector("#mre_share_memberships_attributes_new_record_friend_id");
+
+let frm = document.querySelector(".edit_space_template") || document.querySelector(".edit_kit") || document.querySelector(".edit_space") || document.querySelector(".edit_mre");
+
+[].forEach.call(  members.querySelectorAll("option")  , function(option){
+	if(option.value != ""){
+		let field = document.createElement("input");
+		field.setAttribute("type","hidden");
+		field.setAttribute("value",option.value);
+		field.setAttribute("name",members.name);
+		frm.appendChild(field);
+	}
+})
+
+members.remove();
+
+alert(members.length + ' friends have been added.');
