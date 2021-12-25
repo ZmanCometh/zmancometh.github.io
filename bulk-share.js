90bd1b580753d097b39223fc315c6c05054b03a4
ZmanCometh <88464073+ZmanCometh@users.noreply.github.com>
2021-12-24 18:26:28 -0600

    Update bulk-share.js

--- bulk-share.js
+++ bulk-share.js
@@ -3,9 +3,11 @@ var addfriend = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDER
 
 addfriend.click();
 
-members = document.querySelector("#space_share_memberships_attributes_new_record_friend_id") || document.querySelector("#kit_share_memberships_attributes_new_record_friend_id") || document.querySelector("#mre_share_memberships_attributes_new_record_friend_id");
+members = document.querySelector("#space_share_memberships_attributes_new_record_friend_id") || document.querySelector("#kit_share_memberships_attributes_new_record_friend_id") || document.querySelector("#mre_share_memberships_attributes_new_record_friend_id") || document.querySelector("#space_template_share_memberships_attributes_new_record_friend_id");
 
 let frm = document.querySelector(".edit_space_template") || document.querySelector(".edit_kit") || document.querySelector(".edit_space") || document.querySelector(".edit_mre");
+
+console.log(members);
 
 [].forEach.call(  members.querySelectorAll("option")  , function(option){
 	if(option.value != ""){
